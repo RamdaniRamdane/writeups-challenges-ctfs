2b6b823dc51651953f045004f652fac482e94b55
RamdaniRamdane <ramdaniramdane123+1@gmail.com>
2025-12-26 19:45:26 +0100

    http server

--- root-me/challenges/web-client/websocket0/server.js
+++ root-me/challenges/web-client/websocket0/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 http.createServer((req, res) => {
-  fs.readFile("index.html", (err, data) => {
+  fs.readFile("exploit.html", (err, data) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(data);
   });
